742295774bb62e8fed54e16682ccfec18894db47
include-JoannaLopez <118097227+include-JoannaLopez@users.noreply.github.com>
2023-09-12 00:13:36 -0500

    installed npm packages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Matt Delac
+            Joanna Lopez
           </a>{" "}
           and is{" "}
           <a
