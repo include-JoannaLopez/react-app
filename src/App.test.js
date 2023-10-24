1cb1ae6947153cbb8ed1cad809433a84f63fd1f1
include-JoannaLopez <118097227+include-JoannaLopez@users.noreply.github.com>
2023-10-23 22:50:51 -0500

    updated dictionary app

--- src/App.test.js
+++ src/App.test.js
@@ -26,3 +26,11 @@ export default function FormattedDate(props) {
     </div>
   );
 }
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
